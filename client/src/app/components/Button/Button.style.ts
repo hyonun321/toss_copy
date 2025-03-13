98860b3e01fe4ab7192565fd216f3dbf44688e1f
hyonun321 <hyo_nun@kakao.com>
2025-03-14 01:32:55 +0900

    style: 버튼 눌림상태 구현
    - 토스와 비슷하게 크기 3% 축소
    - 밝기 20% 감소

--- client/src/app/components/Button/Button.style.ts
+++ client/src/app/components/Button/Button.style.ts
@@ -9,15 +9,23 @@ export const StyledButton = styled.button<{
   width: 100%;
   padding: 12px 24px;
   border: none;
-  border-radius: 9999px;
+  border-radius: 20px;
   cursor: pointer;
   background-color: ${({ backgroundColor }) => backgroundColor};
   color: ${({ textColor }) => textColor};
   font-size: 20px;
   font-weight: 500;
-  transition: opacity 0.2s ease;
+  transition:
+    transform 0.2s ease,
+    filter 0.2s ease,
+    opacity 0.2s ease;
 
   &:hover {
     opacity: 0.9;
+  }
+
+  &:active {
+    transform: scale(0.97);
+    filter: brightness(0.8);
   }
 `;
