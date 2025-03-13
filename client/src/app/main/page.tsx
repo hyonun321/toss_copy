2a7b0f2bbd0a3b867e72bd675ddafe3beb9ebe58
hyonun321 <hyo_nun@kakao.com>
2025-03-14 01:33:25 +0900

    chore: 기초 레이아웃용으로 화면 구현
    - 추후 가운데 정렬, 각 화면 별 컨테이너 추가할 예정

--- client/src/app/main/page.tsx
+++ client/src/app/main/page.tsx
@@ -1,14 +1,48 @@
 'use client';
 import { Button } from '../components/Button/Button';
 import styles from './page.module.css';
-
+import { ImageText } from '@/app/components/ImageText/ImageText';
 export default function Main() {
   return (
     <div className={styles.container}>
       <div className={styles.scrollArea}>
-        <h1>주식의 모든 것</h1>
-        <h2>도스에서 간편하게</h2>
-        <p>스크롤되는 내용 예시입니다. 원하는 요소들을 여기에 배치하세요.</p>
+        <div style={{ height: '300px' }} />
+        <ImageText
+          imageSrc="/images/doss_logo.png"
+          alt="도스 로고"
+          text={
+            <>
+              주식의 모든 것
+              <br />
+              도스에서 간편하게
+            </>
+          }
+        />
+        <div style={{ height: '300px' }} />
+        <ImageText
+          imageSrc="/images/storage.png"
+          alt="도스 금고"
+          text={
+            <>
+              보고 싶은 주식을
+              <br />
+              마음 껏 담으세요
+            </>
+          }
+        />
+        <div style={{ height: '300px' }} />
+        <ImageText
+          imageSrc="/images/wallet.png"
+          alt="도스 지갑"
+          text={
+            <>
+              복잡한 검색 없이
+              <br />
+              내주식을 확인해요
+            </>
+          }
+        />
+        <div style={{ height: '300px' }} />
       </div>
 
       <div className={styles.fixedButton}>
