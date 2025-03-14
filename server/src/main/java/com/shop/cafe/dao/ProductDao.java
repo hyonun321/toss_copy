e4ce8ca7ff0ec335aff94cadccfb50f2ce6863dc
hyonun321 <hyo_nun@kakao.com>
2025-03-14 17:19:03 +0900

    refactor: ProductDao 인터페이스화
    - mybatis 사용에 따라 수정

--- server/src/main/java/com/shop/cafe/dao/ProductDao.java
+++ server/src/main/java/com/shop/cafe/dao/ProductDao.java
@@ -1,34 +1,18 @@
 package com.shop.cafe.dao;
 
-import java.sql.*;
 import java.util.*;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
+
 
 import com.shop.cafe.dto.Product;
 
-@Component
-public class ProductDao {
+@Mapper
+public interface ProductDao {
 	
-	public List<Product> getAllProducts() throws Exception{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		try(
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ureca?serverTimezone=UTC","root","1235");
-			PreparedStatement stmt=con.prepareStatement("select * from product");
-			ResultSet rs=stmt.executeQuery();
-				) {	
-			
-			List<Product> list=new ArrayList<>();
-			while(rs.next()) {
-				int prodcode=rs.getInt("prodcode");
-				String prodname=rs.getString("prodname");
-				int price=rs.getInt("price");
-				String pimg=rs.getString("pimg");
-				String ptag=rs.getString("ptag");
-				list.add(new Product( prodcode, price,prodname, pimg,ptag));
-			}
-			return list;
-		}		
-	}	
+	
+	public  List<Product> getAllProducts() throws Exception;
+
+	
 	
 }
