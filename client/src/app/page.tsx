423b272ccd24afd3579ac6b77d68f6a1830262e3
hyonun321 <hyo_nun@kakao.com>
2025-03-17 10:49:30 +0900

    refactor: 하이드레이션 방지 ssr off

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,24 @@
 'use client';
 
-import { MainView } from './components/MainView/MainView';
-import { HomeView } from './components/HomeView/HomeView';
+import dynamic from 'next/dynamic';
 
+const DynamicHomeView = dynamic(
+  () => import('./home/page').then((mod) => mod.default),
+  { ssr: false }, // 하이드레이션 방지 : 서버 사이드 렌더링 비활성화
+);
+const DynamicMainView = dynamic(
+  () => import('./main/page').then((mod) => mod.default),
+  { ssr: false }, // 하이드레이션 방지 : 서버 사이드 렌더링 비활성화
+);
 /*
   실제로는 서버나 쿠키, 세션 등을 통해 로그인 여부를 확인하지만,
   여기서는 간단히 boolean 값으로 대체한다.
 */
-const isLoggedIn = true;
+const isLoggedIn = false;
 
 export default function Page() {
   if (!isLoggedIn) {
-    return <MainView />;
+    return <DynamicMainView />;
   }
-  return <HomeView />;
+  return <DynamicHomeView />;
 }
