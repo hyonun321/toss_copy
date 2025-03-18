9b23253cb646782dea2426d48075c6a3e7c0b1db
hyonun321 <hyo_nun@kakao.com>
2025-03-18 11:25:34 +0900

    feat: 서버에러 메세지 표시 속성 추가 및 연타방지를위한 isLoading 추가

--- client/src/app/register/registerview.tsx
+++ client/src/app/register/registerview.tsx
@@ -13,6 +13,8 @@ type RegisterProps = {
   onNext: (value: string) => void;
   buttonText: string;
   validationType: ValidationType;
+  isLoading?: boolean;
+  serverError?: string | null; // 서버 에러 메시지를 위한 선택적 prop 추가
 };
 
 const REGISTER: React.FC<RegisterProps> = ({
@@ -21,6 +23,8 @@ const REGISTER: React.FC<RegisterProps> = ({
   onNext,
   buttonText,
   validationType,
+  isLoading = false,
+  serverError,
 }) => {
   const [resizeHeight, setResizeHeight] = useState<number>(0);
   const { value, error, handleChange } = useValidation(validationType);
@@ -39,7 +43,7 @@ const REGISTER: React.FC<RegisterProps> = ({
   }, []);
 
   const handleNext = () => {
-    if (!error && value) {
+    if (!error && value && !isLoading) {
       onNext(value);
     }
   };
@@ -58,6 +62,9 @@ const REGISTER: React.FC<RegisterProps> = ({
           onChange={handleChange}
         />
         {error && <p className={styles.errorMessage}>{error}</p>}
+        {!error && serverError && (
+          <p className={styles.errorMessage}>{serverError}</p>
+        )}
       </div>
 
       <div
