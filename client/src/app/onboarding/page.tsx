3c2e56bb9036c242a2c3d026deffa962f101ce6e
hyonun321 <hyo_nun@kakao.com>
2025-03-18 11:26:23 +0900

    chore: format 설정

--- client/src/app/onboarding/page.tsx
+++ client/src/app/onboarding/page.tsx
@@ -83,7 +83,11 @@ export default function Onboarding() {
           transition={{ duration: 0.5, ease: 'easeInOut' }}
           className={styles.slide}
         >
-          <ImageText imageSrc={steps[step].imageSrc} text={steps[step].text} enableAnimation = {false}  />
+          <ImageText
+            imageSrc={steps[step].imageSrc}
+            text={steps[step].text}
+            enableAnimation={false}
+          />
         </motion.div>
       </AnimatePresence>
     </div>
