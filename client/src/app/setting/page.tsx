8d7013be9cc390f5c0a5fe15959d9a6ef167e38c
mstagon <devcms21@gmail.com>
2025-03-18 11:28:48 +0900

    feat: 토글 및 화살표 버튼 이벤트 추가

--- client/src/app/setting/page.tsx
+++ client/src/app/setting/page.tsx
@@ -1,21 +1,41 @@
 'use client';
 
+import { useRouter } from 'next/router';
 import { TopBar } from '@/app/components/SettingTopbar/TopBar';
 import { UserInfo } from '@/app/components/UserInfo/UserInfo';
 import { Option } from '@/app/components/Option/Option';
 import styles from './page.module.css';
 
 const SettingPage = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.settingPage}>
       <TopBar />
       <UserInfo username="{유저닉네임}" email="test@doss.ui" />
       <div className={styles.divider} />
       <div className={styles.content}>
-        <Option iconSrc="/images/moon.png" label="다크 모드" hasToggle={true} />
-        <Option iconSrc="/images/user.png" label="닉네임 변경" />
-        <Option iconSrc="/images/lock.png" label="비밀번호 변경" />
-        <Option iconSrc="/images/unlock.png" label="로그아웃" />
+        <Option
+          iconSrc="/images/moon.png"
+          label="다크 모드"
+          hasToggle={true}
+          onToggleChange={() => console.log('다크모드')}
+        />
+        <Option
+          iconSrc="/images/user.png"
+          label="닉네임 변경"
+          onClick={() => router.push('/password-change')}
+        />
+        <Option
+          iconSrc="/images/lock.png"
+          label="비밀번호 변경"
+          onClick={() => router.push('/password-change')}
+        />
+        <Option
+          iconSrc="/images/unlock.png"
+          label="로그아웃"
+          onClick={() => router.push('/password-change')}
+        />
       </div>
     </div>
   );
