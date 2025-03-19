8ba751f57e7ebc82903f424b8dfb0d92c0f73712
hyonun321 <hyo_nun@kakao.com>
2025-03-19 14:22:50 +0900

    feat: 아무것도 없는 /* 라우트로 들어올때 authToken을 체크한다. 토큰이 유효하면 home 화면으로 이동, 토큰이 없거나 유효하지않으면 main 화면으로 이동시킴

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import dynamic from 'next/dynamic';
 import { isAuthenticated } from './utils/auth';
-
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 const DynamicHomeView = dynamic(
   () => import('./home/page').then((mod) => mod.default),
   { ssr: false }, // 하이드레이션 방지 : 서버 사이드 렌더링 비활성화
@@ -13,10 +14,36 @@ const DynamicMainView = dynamic(
 );
 
 export default function Page() {
-  const isLoggedIn = isAuthenticated();
+  const [authStatus, setAuthStatus] = useState<boolean | null>(null);
 
-  if (!isLoggedIn) {
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authenticated = await isAuthenticated();
+      setAuthStatus(authenticated);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (authStatus === null) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          width: '100%',
+        }}
+      >
+        <Image width={100} height={100} src="/images/loading.gif" />
+      </div>
+    );
+  }
+
+  if (!authStatus) {
     return <DynamicMainView />;
   }
+
   return <DynamicHomeView />;
 }
