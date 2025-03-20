b6cba71ad8a3d2fd7c714f9887acfea442335a0b
hyonun321 <hyo_nun@kakao.com>
2025-03-20 12:53:47 +0900

    feat: DB에 거래소,종목코드,종목명, 이메일 전부 들어가게 수정
    - 사용자의 주식코드 목록을 가져옴
    - 각 좋아요 목록에서 코드,종목명,거래소 정보추출
    - 각 주식의 가격정보를 가져온다
    - 데이터를 캐싱해놨으면 먼저 주고 아닐경우 api 접근

--- server/src/main/java/com/shop/cafe/dto/Like.java
+++ server/src/main/java/com/shop/cafe/dto/Like.java
@@ -1,37 +1,20 @@
 package com.shop.cafe.dto;
 
-public class Like{
+public class Like {
     private String email;
     private String stockCode;
-
-    public Like() {
-        super();
-    }
-
-    public Like(String email, String stockCode) {
-        super();
-        this.email = email;
-        this.stockCode = stockCode;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStockCode() {
-        return stockCode;
-    }
-
-    public void setStockCode(String stockCode) {
-        this.stockCode = stockCode;
-    }
-
-    @Override
-    public String toString() {
-        return "LikeDTO [email=" + email + ", stockCode=" + stockCode + "]";
-    }
+    private String stockName;
+    private String exchangeCode; // 추가된 필드
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getStockCode() { return stockCode; }
+    public void setStockCode(String stockCode) { this.stockCode = stockCode; }
+    
+    public String getStockName() { return stockName; }
+    public void setStockName(String stockName) { this.stockName = stockName; }
+    
+    public String getExchangeCode() { return exchangeCode; }
+    public void setExchangeCode(String exchangeCode) { this.exchangeCode = exchangeCode; }
 }
